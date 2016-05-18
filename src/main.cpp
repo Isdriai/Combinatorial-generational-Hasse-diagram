30ce2cd044e9183db10e237bf8021c74bbe483dc
Rodolphe_Picot <picot.rodolphe@gmail.com>
2016-05-18 09:50:51 +0200

    rank ok without advanced test

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,37 @@
 #include <iostream>
 #include <bitset>
+#include <vector>
 
 using namespace std;
 
 void generate(int, int);
 void list();
-void rank();
-void unrank();
-void previous();
+int unrank();
+int previous();
+int next();
 
 const int puissance=4;
 const int count = 2 << puissance ;
 
-bitset<puissance> solutions [count];
+
+
+vector<vector<bitset<puissance>>> solutions (puissance+1);
+
+void affiche () {
+	for ( std::vector<bitset<puissance>> elem : solutions){
+		for (bitset<puissance> e : elem)
+		{
+			cout << e << endl ;
+		}
+	}
+}
+
+template <size_t puissance>
+
 
 void generate(int run, int endroit,bitset<puissance> acc){
 	if (run == 0 ){
-		solutions[endroit]=acc;
-		cout << acc << endl ;
+		solutions[endroit].push_back(acc);
 	}
 	else {
 
@@ -25,7 +39,7 @@ void generate(int run, int endroit,bitset<puissance> acc){
 		un[run-1]=1;
 
 		generate(run-1, endroit, acc);
-		generate(run-1, endroit+ (puissance-run) << 1, un);
+		generate(run-1, endroit+1, un);
 	}
 }
 
@@ -33,21 +47,70 @@ void list(){
 
 }
 
-void rank(){
+int factorial(int n){
+	int tmp = 1;
+
+	for (int i = 1; i <= n; ++i)
+	{
+		tmp*=i;
+	}
+
+	return tmp;
+}
+
+int binomial(int n, int k){
+	return factorial(n)/(factorial(k)*(factorial(n-k)));
+}
+
+int parcour(bitset<puissance> elem, int reste, int min, int max, int zero, int un){
+	if(reste == 0 ){
+		return min; // we can return max, it's the same thing
+	}
+	else {
+		if (elem[reste]){
+			int bin = binomial(reste, un-1);
+			return parcour(elem, reste-1, min+bin , max , zero, un-1);
+		}
+		else{
+			int bin = binomial(reste, zero-1);
+			return parcour(elem, reste-1, min , max-bin , zero-1, un);
+		}
+				
+	}
+}
+
+int ranka(bitset<puissance> elem){
+
+	int c0 = 0 ;
+
+	for (int i = 0; i < puissance ; i++){
+		if ( elem[i] == 0) {
+			c0++;
+		}
+	}
+
+ 	return parcour(elem, puissance, 0, binomial(puissance, puissance-c0), c0, puissance-c0);
 
 }
 
-void unrank(){
+int unrank(){
+	
+}
+
+int previous(){
 
 }
 
-void previous(){
-
+int next(){
+	
 }
 
 int main(int argc, char const *argv[])
 {
+	bitset<puissance> test = bitset<puissance> (5);
 	bitset<puissance> tmp;
-	generate(puissance, 0, tmp);
+	// generate(puissance, 0, tmp);
+	cout << ranka(test) << endl ;
+	affiche();
 	return 0;
 }
